6031bcfe207daa87ea4dd129ff7a7962fe6fa6d9
thomaslnx <marcos.msilva10@gmail.com>
2019-11-27 09:58:34 -0300

    Header Menu

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,7 +7,6 @@ export const Header = styled.div`
   position: fixed;
   display: flex;
   flex-direction: row;
-  border-bottom: 1px solid #333;
 `;
 
 export const Logo = styled.image`
@@ -18,7 +17,7 @@ export const Logo = styled.image`
 
 export const Menu = styled.div`
   position: realtive;
-  margin-left: 0px;
+  margin-left: 60px;
   margin-top: 0px;
   width: 100%;
   display: flex;
@@ -26,11 +25,11 @@ export const Menu = styled.div`
   align-items: center;
 
   p {
-    color: #6e9ecc;
+    color: #449aaa;
   }
 
   button {
-    background-color: #1d5e1e;
+    background-color: #6acc30;
     height: 50px;
     width: 300px;
     border-radius: 4px;
